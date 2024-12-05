2c43d94250c99543fabbea88d01fa8a77bb2d649
VIKAS <vikasraw17@gmail.com>
2024-12-05 22:46:54 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
-import Users from "./components/Users";
+import Users from "./components/User";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        {/* Public Route */}
         <Route path="/" element={<Login />} />
-        <Route path="/users" element={<Users />} />
+        
+        {/* Protected Route */}
+        <Route
+          path="/users"
+          element={
+            <ProtectedRoute>
+              <Users />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
